7354134e33a8a2f26cd5e900f02ca0c3f8743f63
Advait-Wakulkar <wakulkar.a@northeastern.edu>
2025-06-18 05:25:37 -0400

    feat: implement layout and homepage structure; add tech icons display and interview card components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,15 @@
- export default function Layout({ children }: { children: React.ReactNode }) {
+ import Image from "next/image"
+ import Link from "next/link"
+ 
+ export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+      <div className="root-layout">
+        <nav>
+          <Link href="/" className="flex items-center gap-2">
+          <Image src="logo.svg" alt="logo" height={32} width={38}></Image>
+          <h2 className="text-primary-100">Inprep</h2>
+          </Link>
+        </nav>
       {children}
     </div>
   )
